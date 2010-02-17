f72d88956dd6e8d411684931821c56cd2356ff46
Sven Herzberg <herzi@gnome-de.org>
2010-02-18 00:02:36 +0100

    improve the visual appearance of the window menu items
    * window-menu-item.c: improve the visual layout

--- window-menu-item.c
+++ window-menu-item.c
@@ -44,15 +44,41 @@ window_item_activated (GtkWidget * widget,
 GtkWidget*
 window_menu_item_new (WnckWindow* window)
 {
+  WnckApplication* application;
+  GdkPixbuf* pixbuf;
   GtkWidget* item;
+  GtkWidget* label;
+  gchar    * markup;
 
   g_return_val_if_fail (WNCK_IS_WINDOW (window), NULL);
 
   item = gtk_image_menu_item_new ();
-  gtk_menu_item_set_label (GTK_MENU_ITEM (item),
-                           wnck_window_get_name (window));
+
+  application = wnck_window_get_application (window);
+
+  /* FIXME: track icon changes */
+  pixbuf = wnck_application_get_icon (application);
+  if (pixbuf)
+    {
+      GtkWidget* image = gtk_image_new_from_pixbuf (pixbuf);
+      gtk_widget_show (image);
+      gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (item), image);
+    }
+
   g_signal_connect (item, "activate",
                     G_CALLBACK (window_item_activated), window);
+
+  /* FIXME: track name changes */
+  label = gtk_label_new (NULL);
+  gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
+  markup = g_strdup_printf ("<b>%s</b>\n<small>%s</small>",
+                            wnck_application_get_name (application),
+                            wnck_window_get_name (window));
+  gtk_label_set_markup (GTK_LABEL (label), markup);
+  g_free (markup);
+
+  gtk_widget_show (label);
+  gtk_container_add (GTK_CONTAINER (item), label);
 
   return item;
 }
