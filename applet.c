e927aa97027341f78cd6daec2e221fe73183aa74
Sven Herzberg <herzi@gnome-de.org>
2010-02-19 22:39:33 +0100

    let the applet expand
    * applet.c: let the applet expand

--- applet.c
+++ applet.c
@@ -10,19 +10,20 @@ hello_applet_fill (PanelApplet *applet,
                    const gchar *iid,
                    gpointer     data)
 {
-        GtkWidget *label;
+  GtkWidget *label;
 
-        if (strcmp (iid, "OAFIID:Herzis_WorkspaceBrowserApplet") != 0)
-                return FALSE;
+  if (strcmp (iid, "OAFIID:Herzis_WorkspaceBrowserApplet") != 0)
+    return FALSE;
 
-        panel_applet_set_flags (applet, PANEL_APPLET_HAS_HANDLE);
+  panel_applet_set_flags (applet,
+                          PANEL_APPLET_EXPAND_MAJOR | PANEL_APPLET_EXPAND_MINOR | PANEL_APPLET_HAS_HANDLE);
 
-        label = workspace_browser_new ();
-        gtk_container_add (GTK_CONTAINER (applet), label);
+  label = workspace_browser_new ();
+  gtk_container_add (GTK_CONTAINER (applet), label);
 
-        gtk_widget_show_all (GTK_WIDGET (applet));
+  gtk_widget_show_all (GTK_WIDGET (applet));
 
-        return TRUE;
+  return TRUE;
 }
 
 
@@ -33,3 +34,4 @@ PANEL_APPLET_BONOBO_FACTORY ("OAFIID:Herzis_WorkspaceBrowserApplet_Factory",
                              hello_applet_fill,
                              NULL);
 
+/* vim:set et sw=2 cino=t0,f0,(0,{s,>2s,n-1s,^-1s,e2s: */
