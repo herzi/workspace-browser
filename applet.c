e4e9268313e04db7fa8c90f7a431267cc743d3ef
Sven Herzberg <herzi@gnome-de.org>
2010-02-19 22:26:05 +0100

    drop the revision stuff
    * Makefile.am: drop the revision stuff; added a silent generation rule
    * applet.c: drop the revision stuff

--- applet.c
+++ applet.c
@@ -3,7 +3,6 @@
 #include <panel-applet.h>
 #include <gtk/gtklabel.h>
 
-#include "applet.h"
 #include "workspace-browser.h"
 
 static gboolean
@@ -19,7 +18,6 @@ hello_applet_fill (PanelApplet *applet,
         panel_applet_set_flags (applet, PANEL_APPLET_HAS_HANDLE);
 
         label = workspace_browser_new ();
-        gtk_widget_set_tooltip_text (label, REVISION);
         gtk_container_add (GTK_CONTAINER (applet), label);
 
         gtk_widget_show_all (GTK_WIDGET (applet));
